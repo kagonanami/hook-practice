499491a125f0a4a51264caa47988be7a9f0e7252
Kago <kagonanami@gmail.com>
2022-07-22 23:33:19 +0700

    b

---
+++ src/component/Context.js
@@ -0,0 +1,10 @@
+import React, {createContext, useContext} from 'react';
+
+export default function Context() {
+    const themeContext = createContext()
+  return (
+    <div>
+      
+    </div>
+  );
+}
